9a3174e00e975e57552d30e6bbe96ddecfd88b53
Anish Mathakiya <anumathakiya073@gmail.com>
2022-12-23 02:18:13 +0530

    Update

--- sum.java
+++ sum.java
@@ -2,6 +2,6 @@ class sum{
     public static void main(String[] args){
         int a=2;
         int b=3;
-        System.out.println("sum is "+(a+b));
+        System.out.println("here the sum is "+(a+b));
     }
 }
